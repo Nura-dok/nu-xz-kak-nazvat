4287ee1c05869904f0c02c2497aa52a40ef0b96e
Nura-dok <75525513+Nura-dok@users.noreply.github.com>
2021-10-22 12:32:14 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 
-// for(let i=1; i<=10000; i++) {if(String(i).includes('222')){ console.log(i);}}
+// for(i=1; i<=10000; i++) {
+//     if(String(i).includes('222')){ 
+//         console.log(i);}}
 
-for(let i=1; i<=100; i++) {
-    if(i%Math.sqrt(i)==0){
-    console.log(i);
-    }  
-}
+// for(i=1; i<=100; i++) {
+//     if(i%Math.sqrt(i)==0){
+//     console.log(i);
+//     }  
+function a(n){
+for (i = 1; i <= n; i++) {
+	var res = [];
+	for (a = 1; a <= i; a++) {                                                  // https://learn.javascript.ru/array
+		res.push(i);
+	}
+	console.log(res.join(''));
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
